ba629d348bf10db8621f1fbbb3e5a336927f0203
Bharath Ashok <iambharath.ashoka@gmail.com>
2024-02-27 23:04:54 +0530

    Comment Resource Update API Implementation Changes

--- src/main/java/com/bharath/leanring/blog/socialmediablogapp/controller/CommentController.java
+++ src/main/java/com/bharath/leanring/blog/socialmediablogapp/controller/CommentController.java
@@ -1,6 +1,8 @@
 package com.bharath.leanring.blog.socialmediablogapp.controller;
 
 import com.bharath.leanring.blog.socialmediablogapp.dto.CommentDto;
+import com.bharath.leanring.blog.socialmediablogapp.dto.PatchDto;
+import com.bharath.leanring.blog.socialmediablogapp.entity.Comment;
 import com.bharath.leanring.blog.socialmediablogapp.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -32,6 +34,44 @@ public class CommentController {
         List<CommentDto> commentDtoList = commentService.getAllCommentsByPostId(postId);
         return new ResponseEntity<>(commentDtoList, HttpStatus.OK);
     }
+
+
+    //GET Comment By CommentId and PostId - /api/v1/posts/{postId}/comments/{id}
+    @GetMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> fetchCommentByPostIdAndCommentId(@PathVariable("postId") Long postId, @PathVariable("id") Long id) {
+        CommentDto commentDto =  commentService.getCommentByPostIdAndCommentId(postId, id);
+        return new ResponseEntity<>(commentDto, HttpStatus.OK);
+    }
+
+   // PUT Update Comment By PostId and CommentId /api/v1/posts/{postId}/comments/{id}
+   @PutMapping("/posts/{postId}/comments/{id}")
+   public ResponseEntity<CommentDto> updateCommentByPostIdAndCommentId(@PathVariable("postId") Long postId,
+                                                                       @PathVariable("id") Long id,
+                                                                       @RequestBody CommentDto commentDto) {
+       CommentDto updatedCommentDto =  commentService.updateCommentByPostIdAndCommentId(postId, id, commentDto);
+       return new ResponseEntity<>(updatedCommentDto, HttpStatus.OK);
+   }
+
+
+   //Delete Comment By CommentId and PostId - /api/v1/posts/{postId}/comments/{id}
+
+
+    //Patch Comment By CommentId and PostId
+    @PatchMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> partiallyUpdateCommentByPostIdAndCommentId(@PathVariable("postId") Long postId,
+                                                                        @PathVariable("id") Long id,
+                                                                        @RequestBody PatchDto patchDto) {
+        CommentDto updatedCommentDto = null;
+        if(patchDto.getOperation().equalsIgnoreCase("update")) {
+            updatedCommentDto =  commentService.updateCommentPartiallyByPostIdAndCommentId(postId, id, patchDto);
+        } else if (patchDto.getOperation().equalsIgnoreCase("delete")) {
+            //updatedCommentDto =  commentService.deleteParticularField(postId, id, patchDto);
+        }
+        return new ResponseEntity<>(updatedCommentDto, HttpStatus.OK);
+    }
+
+
+
 
 
 }
--- src/main/java/com/bharath/leanring/blog/socialmediablogapp/service/CommentService.java
+++ src/main/java/com/bharath/leanring/blog/socialmediablogapp/service/CommentService.java
@@ -1,6 +1,7 @@
 package com.bharath.leanring.blog.socialmediablogapp.service;
 
 import com.bharath.leanring.blog.socialmediablogapp.dto.CommentDto;
+import com.bharath.leanring.blog.socialmediablogapp.dto.PatchDto;
 
 import java.util.List;
 
@@ -9,7 +10,9 @@ public interface CommentService {
     CommentDto createComment(long postId, CommentDto commentDto);
 
     List<CommentDto> getAllCommentsByPostId(long postId);
+    CommentDto getCommentByPostIdAndCommentId(long postId,long id);
 
+    CommentDto updateCommentByPostIdAndCommentId(long postId, long id, CommentDto commentDto);
 
-
+    CommentDto updateCommentPartiallyByPostIdAndCommentId(Long postId, Long id, PatchDto patchDto);
 }
